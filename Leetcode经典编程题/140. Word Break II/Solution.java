63d164872c5c8147ad22ca5536d931ede3662c08
gZeyu <gzy8535@gmail.com>
2019-05-22 23:59:17 +0800

    0522v2

--- Leetcode经典编程题/140. Word Break II/Solution.java
+++ Leetcode经典编程题/140. Word Break II/Solution.java
@@ -1,17 +1,17 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 class Solution {
 
   public List<String> wordBreak(String s, List<String> wordDict) {
     List<List<String>> cache = new ArrayList<>();
-    for (int i = 0; i < s.length(); i++) {
-      cache.add(new ArrayList<>());
-    }
     wordBreakCore(s, wordDict, 0, cache);
     return cache.get(0);
   }
+
+  private int a = 0;
 
   private void wordBreakCore(String s, List<String> wordDict, int begin, List<List<String>> cache) {
     if (begin == s.length()) {
@@ -20,17 +20,28 @@ class Solution {
     if (cache.get(begin).size() > 0) {
       return;
     }
-    System.out.println(begin + "," + cache.get(begin));
+    a++;
     for (String word : wordDict) {
       int end = begin + word.length();
+      // System.out.println(
+      // begin
+      // + ","
+      // + end
+      // + ","
+      // + s.substring(begin, end)
+      // + ","
+      // + word.equals(s.substring(begin, end)));
       if (end <= s.length() && word.equals(s.substring(begin, end))) {
+        if (a == 70) {
+          System.out.println(a);
+        }
         wordBreakCore(s, wordDict, end, cache);
         if (end < s.length()) {
           for (String result : cache.get(end)) {
             cache.get(begin).add(word + " " + result);
           }
         } else {
-          cache.get(begin).add(word);
+          cache.set(begin, Collections.singletonList(word));
         }
       }
     }
@@ -38,10 +49,16 @@ class Solution {
 
   public static void main(String[] args) {
     Solution s = new Solution();
-    System.out.println(s.wordBreak("catsanddog", Arrays.asList("cat", "cats", "and", "sand", "dog")));
-    System.out
-        .println(s.wordBreak("pineapplepenapple", Arrays.asList("apple", "pen", "applepen", "pine", "pineapple")));
-    System.out.println(s.wordBreak("catsandog", Arrays.asList("cats", "dog", "sand", "and", "cat")));
+    // System.out.println(
+    // s.wordBreak("catsanddog", Arrays.asList("cat", "cats", "and", "sand",
+    // "dog")));
+    // System.out.println(
+    // s.wordBreak(
+    // "pineapplepenapple", Arrays.asList("apple", "pen", "applepen", "pine",
+    // "pineapple")));
+    // System.out.println(
+    // s.wordBreak("catsandog", Arrays.asList("cats", "dog", "sand", "and",
+    // "cat")));
     System.out.println(s.wordBreak(
         "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
         Arrays.asList("a", "aa", "aaa", "aaaa", "aaaaa", "aaaaaa", "aaaaaaa", "aaaaaaaa", "aaaaaaaaa", "aaaaaaaaaa")));
